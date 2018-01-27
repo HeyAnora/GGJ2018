e17e298025ea73bacca23ac0bb72e19e41aa3f0b
albertal <albertal@miamioh.edu>
2018-01-27 00:45:06 -0500

    End of Day 1

---
+++ Assets/Scripts/Censor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Censor : MonoBehaviour
+{
+
+    private void ActivateCensor()
+    {
+        Debug.Log("Censoring");
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("space"))
+        { 
+            ActivateCensor();
+        }
+	}
+}
---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    //[Serializable]
+    public class StatementTime
+    {
+        public float intro;
+        public float outro;
+    }
+
+    public List<StatementTime> DataList;
+
+    private void Awake()
+    {
+        InitiateData();
+    }
+
+    private static string GetDialogueData()
+    {
+        TextAsset file = Resources.Load("DialogueData") as TextAsset;
+        return file.text;
+    }
+
+    private void InitiateData()
+    {
+        string dialogueText = GetDialogueData();
+        string[] list = dialogueText.Split("\n"[0]);
+
+        foreach (string line in list)
+        {
+            string[] split = line.Split('/');
+            float introTime = float.Parse(split[0].Trim());
+            float outroTime = float.Parse(split[1].Trim());
+
+            StatementTime newStatement = new StatementTime()
+            {
+                intro = introTime,
+                outro = outroTime
+            };
+
+            DataList.Add(newStatement);
+        }
+
+    }
+
+    ///TO DO: 
+    ///add way to check list timings
+    ///check for each set of timings at a time, after one is checked begin chekcing next one
+
+
+}
